b6cadbc87063eb885e24522403ebca0a7d0960f4
Nirmal Prajapat <prajapatnirmal168@gmail.com>
2026-08-26 23:43:19 +0530

    Import crypto explicitly in authController to prevent ReferenceError on legacy node runtimes

--- backend/services/auth/controllers/authController.js
+++ backend/services/auth/controllers/authController.js
@@ -2,6 +2,7 @@ import { getAuth } from 'firebase-admin/auth'
 import { app } from '../config/firebase.js'
 import User from '../models/user.model.js'
 import redis from '../shared/redis/redis.js'
+import crypto from 'crypto'
 
 export const login = async (req, res) => {
   try {
